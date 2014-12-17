e228d0e0a246c2feed747128f29ed4054bf56329
Dries Vints <dries.vints@gmail.com>
2014-12-17 16:36:28 +0100

    Update with Scrutinizer patches

--- src/LockServiceProvider.php
+++ src/LockServiceProvider.php
@@ -99,7 +99,7 @@ class LockServiceProvider extends ServiceProvider
     /**
      * Get the services provided by the provider
      *
-     * @return array
+     * @return string[]
      */
     public function provides()
     {
